cb6eed98092a0304e39c089fba07ebb08c2f7fd8
Kevin Wang <kevmo314@gmail.com>
2024-01-02 00:25:41 -0500

    feat: add rtsp server sink

--- app/src/main/go/go.mod
+++ app/src/main/go/go.mod
@@ -3,18 +3,20 @@ module github.com/kevmo314/kinetic
 go 1.21.0
 
 require (
-	github.com/google/uuid v1.3.1
+	github.com/bluenviron/gortsplib/v4 v4.6.2
+	github.com/google/uuid v1.4.0
+	github.com/pion/interceptor v0.1.25
+	github.com/pion/rtp/v2 v2.0.0
 	github.com/pion/transport/v3 v3.0.1
 	github.com/pion/webrtc/v4 v4.0.0-beta.7
-	inet.af/netaddr v0.0.0-20230525184311-b8eac61e914a
 )
 
 require (
+	github.com/bluenviron/mediacommon v1.5.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pion/datachannel v1.5.5 // indirect
 	github.com/pion/dtls/v2 v2.2.7 // indirect
 	github.com/pion/ice/v3 v3.0.2 // indirect
-	github.com/pion/interceptor v0.1.25 // indirect
 	github.com/pion/logging v0.2.2 // indirect
 	github.com/pion/mdns v0.0.9 // indirect
 	github.com/pion/randutil v0.1.0 // indirect
@@ -28,13 +30,8 @@ require (
 	github.com/pion/turn/v3 v3.0.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
-	go4.org/intern v0.0.0-20211027215823-ae77deb06f29 // indirect
-	go4.org/unsafe/assume-no-moving-gc v0.0.0-20230525183740-e7c30c78aeb2 // indirect
 	golang.org/x/crypto v0.16.0 // indirect
-	golang.org/x/mobile v0.0.0-20231127183840-76ac6878050a // indirect
-	golang.org/x/mod v0.14.0 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
-	golang.org/x/tools v0.16.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ app/src/main/go/rtsp_server.go
@@ -0,0 +1,197 @@
+package kinetic
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/pion/rtp"
+	"github.com/pion/rtp/codecs"
+	"github.com/pion/webrtc/v4/pkg/media/h264reader"
+
+	"github.com/bluenviron/gortsplib/v4"
+	"github.com/bluenviron/gortsplib/v4/pkg/base"
+	"github.com/bluenviron/gortsplib/v4/pkg/description"
+	"github.com/bluenviron/gortsplib/v4/pkg/format"
+)
+
+type RTSPServerSink struct {
+	s         *gortsplib.Server
+	mutex     sync.Mutex
+	stream    *gortsplib.ServerStream
+	publisher *gortsplib.ServerSession
+}
+
+// called when a connection is opened.
+func (sh *RTSPServerSink) OnConnOpen(ctx *gortsplib.ServerHandlerOnConnOpenCtx) {
+	log.Printf("conn opened")
+}
+
+// called when a connection is closed.
+func (sh *RTSPServerSink) OnConnClose(ctx *gortsplib.ServerHandlerOnConnCloseCtx) {
+	log.Printf("conn closed (%v)", ctx.Error)
+}
+
+// called when a session is opened.
+func (sh *RTSPServerSink) OnSessionOpen(ctx *gortsplib.ServerHandlerOnSessionOpenCtx) {
+	log.Printf("session opened")
+}
+
+// called when a session is closed.
+func (sh *RTSPServerSink) OnSessionClose(ctx *gortsplib.ServerHandlerOnSessionCloseCtx) {
+	log.Printf("session closed")
+}
+
+// called when receiving a DESCRIBE request.
+func (sh *RTSPServerSink) OnDescribe(ctx *gortsplib.ServerHandlerOnDescribeCtx) (*base.Response, *gortsplib.ServerStream, error) {
+	log.Printf("describe request")
+
+	return &base.Response{
+		StatusCode: base.StatusOK,
+	}, sh.stream, nil
+}
+
+// called when receiving an ANNOUNCE request.
+func (sh *RTSPServerSink) OnAnnounce(ctx *gortsplib.ServerHandlerOnAnnounceCtx) (*base.Response, error) {
+	log.Printf("announce request")
+
+	// this server doesn't accept publishes from another client
+	return &base.Response{
+		StatusCode: base.StatusForbidden,
+	}, nil
+}
+
+// called when receiving a SETUP request.
+func (sh *RTSPServerSink) OnSetup(ctx *gortsplib.ServerHandlerOnSetupCtx) (*base.Response, *gortsplib.ServerStream, error) {
+	log.Printf("setup request")
+
+	return &base.Response{
+		StatusCode: base.StatusOK,
+	}, sh.stream, nil
+}
+
+// called when receiving a PLAY request.
+func (sh *RTSPServerSink) OnPlay(ctx *gortsplib.ServerHandlerOnPlayCtx) (*base.Response, error) {
+	log.Printf("play request")
+
+	return &base.Response{
+		StatusCode: base.StatusOK,
+	}, nil
+}
+
+// called when receiving a RECORD request.
+func (sh *RTSPServerSink) OnRecord(ctx *gortsplib.ServerHandlerOnRecordCtx) (*base.Response, error) {
+	log.Printf("record request")
+
+	return &base.Response{
+		StatusCode: base.StatusForbidden,
+	}, nil
+}
+
+func NewRTSPServerSink() (*RTSPServerSink, error) {
+	h := &RTSPServerSink{}
+	h.s = &gortsplib.Server{
+		Handler:           h,
+		RTSPAddress:       ":8554",
+		UDPRTPAddress:     ":8000",
+		UDPRTCPAddress:    ":8001",
+		MulticastIPRange:  "224.1.0.0/16",
+		MulticastRTPPort:  8002,
+		MulticastRTCPPort: 8003,
+	}
+	return h, h.s.Start()
+}
+
+func (s *RTSPServerSink) AddH264Track() (*RTSPServerSinkH264Track, error) {
+	s.stream = gortsplib.NewServerStream(s.s, &description.Session{
+		Medias: []*description.Media{{
+			Type: description.MediaTypeVideo,
+			Formats: []format.Format{&format.H264{
+				PayloadTyp:        96,
+				PacketizationMode: 1,
+			}},
+		}},
+	})
+	return NewRTSPServerSinkH264Track(s.stream)
+}
+
+type RTSPServerSinkH264Track struct {
+	stream *gortsplib.ServerStream
+
+	buffer *bytes.Buffer
+	reader *h264reader.H264Reader
+
+	packetizer rtp.Packetizer
+
+	ptsMicroseconds int64
+}
+
+func NewRTSPServerSinkH264Track(stream *gortsplib.ServerStream) (*RTSPServerSinkH264Track, error) {
+	// TODO: Should sps/pps be required here?
+	buffer := &bytes.Buffer{}
+	reader, err := h264reader.NewReader(buffer)
+	if err != nil {
+		return nil, err
+	}
+	return &RTSPServerSinkH264Track{
+		stream: stream,
+		buffer: buffer,
+		reader: reader,
+		packetizer: rtp.NewPacketizer(
+			1500,
+			96,
+			0,
+			&codecs.H264Payloader{},
+			rtp.NewRandomSequencer(),
+			90000,
+		),
+	}, nil
+}
+
+func (t *RTSPServerSinkH264Track) WriteH264AnnexBSample(buf []byte, ptsMicroseconds int64) error {
+	// append to buffer
+	if _, err := t.buffer.Write(buf); err != nil {
+		return err
+	}
+	// calculate the duration
+	if t.ptsMicroseconds == 0 {
+		t.ptsMicroseconds = ptsMicroseconds
+	}
+	duration := time.Duration(ptsMicroseconds-t.ptsMicroseconds) * time.Microsecond
+	t.ptsMicroseconds = ptsMicroseconds
+	// drain the reader
+	for i := 0; ; i++ {
+		nalu, err := t.reader.NextNAL()
+		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		}
+		if nalu == nil {
+			return nil
+		}
+		log.Printf("%d bytes, duration %v", len(nalu.Data), duration)
+
+		samples := uint32((33 * time.Millisecond).Seconds() * 90000)
+		packets := t.packetizer.Packetize(nalu.Data, samples)
+		for _, p := range packets {
+			if err := t.stream.WritePacketRTP(&description.Media{
+				Type: description.MediaTypeVideo,
+				Formats: []format.Format{&format.H264{
+					PayloadTyp:        96,
+					PacketizationMode: 1,
+				}},
+			}, p); err != nil {
+				return err
+			}
+		}
+	}
+}
+
+func (s *RTSPServerSink) Close() error {
+	s.s.Close()
+	return nil
+}
