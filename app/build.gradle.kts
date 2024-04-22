b4862a90836c1fc3294f411273ca30600e1788c0
Kevin Wang <kevmo314@gmail.com>
2024-04-22 03:31:44 -0400

    feat: add libuvc native code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,12 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/jni/CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
 }
 
 dependencies {
